4edfbb79f5ff80cb79037689a336c37ee67e21a0
YuviPanda <yuvipanda@gmail.com>
2012-05-28 11:36:20 +0530

    Use cordova instead of PhoneGap in PinchZoom plugin

--- assets/www/android/plugins/PinchZoom.js
+++ assets/www/android/plugins/PinchZoom.js
@@ -2,17 +2,14 @@ var PinchZoom = function() {
 };
 
 PinchZoom.prototype.addEventListener = function(eventName, callback) {
-	console.log('hello');
-	return PhoneGap.exec(callback, null, 'PinchZoom', 'start', []);
+	return cordova.exec(callback, null, 'PinchZoom', 'start', []);
 }
 
 PinchZoom.prototype.removeEventListener = function(eventName, callback) {
 	// vague and not exact :)
-	return PhoneGap.exec(callback, null, 'PinchZoom', 'stop', []);
+	return cordova.exec(callback, null, 'PinchZoom', 'stop', []);
 }
 
-
-PhoneGap.addConstructor(function() {
-	PhoneGap.addPlugin("pinchZoom", new PinchZoom());
+cordova.addConstructor(function() {
+	cordova.addPlugin("pinchZoom", new PinchZoom());
 });
-
