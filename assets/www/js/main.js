4179405246a1228344a4f492a50541486493ad60
Brion Vibber <brion@pobox.com>
2012-06-02 14:55:12 +0200

    More tweaks to make the app work again on both Android 2.x and 4.x
    We don't seem to get the 'deviceready' event fired reliably the way we're loading
    Cordova asynchronously. Just sticking stuff at the end and kinda hoping all is well,
    seems ok so far. :P

--- assets/www/js/main.js
+++ assets/www/js/main.js
@@ -1,20 +1,22 @@
-function onBodyLoad() {
-	// Load Cordova specific stuff here. They all use global variables
-	var GLOBAL_LIBS = [];
-	if (window.CORDOVA_MODULE !== undefined) {
-		GLOBAL_LIBS.push(CORDOVA_MODULE);
-	}
-	if (window.PLATFORM_MODULE !== undefined) {
-		GLOBAL_LIBS.push(PLATFORM_MODULE);
-	}
-	GLOBAL_LIBS.push('mediawiki');
-	if (window.CORDOVA_PLUGINS !== undefined) {
-		GLOBAL_LIBS = GLOBAL_LIBS.concat(CORDOVA_PLUGINS);
-	}
-	require(GLOBAL_LIBS, function() {
-		document.addEventListener('deviceready', onDeviceReady, false);
-	});
+// Load Cordova specific stuff here. They all use global variables
+var GLOBAL_LIBS = [];
+if (window.CORDOVA_MODULE !== undefined) {
+	GLOBAL_LIBS.push(CORDOVA_MODULE);
 }
+if (window.PLATFORM_MODULE !== undefined) {
+	GLOBAL_LIBS.push(PLATFORM_MODULE);
+}
+GLOBAL_LIBS.push('mediawiki');
+if (window.CORDOVA_PLUGINS !== undefined) {
+	GLOBAL_LIBS = GLOBAL_LIBS.concat(CORDOVA_PLUGINS);
+}
+require(GLOBAL_LIBS, function() {
+	// deviceready event never fires on Android 4
+	// perhaps we've already passed it before our async load starts?
+	// we'll just assume we're ready by now for now. Sigh. -- bv 2012-06-02
+	//document.addEventListener('deviceready', onDeviceReady, false);
+	onDeviceReady();
+});
 
 function onDeviceReady() {
 	require(["jquery", "l10n", 'jquery.localize', 'app'], function($, l10n) {
