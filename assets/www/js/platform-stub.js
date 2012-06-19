981d1f55a469c8e410f7b64aed8708f33b2cc5cb
Jon Robson <jdlrobson@gmail.com>
2012-06-19 16:40:10 -0700

    cleanup platform stub
    sort out whitespace, rewrite string concatentation to use array and
    join for readability

--- assets/www/js/platform-stub.js
+++ assets/www/js/platform-stub.js
@@ -1,12 +1,14 @@
+/*global window*/
+/*jslint sloppy: true, white:true, maxerr: 50, indent: 4, plusplus: true, vars:true */
 window.platform = {
-    geoUrl: function(lat, lon, address) {
-    	// Google maps links for web & iOS
-    	// on iOS these open in native Maps app
-        var url = 'http://maps.google.com/maps';
-        url += '?ll=' + lat + ',' + lon;
-        if (address) {
-        	url += '&q=' + encodeURIComponent(address);
-        }
-        return url;
-    }
+	geoUrl: function(lat, lon, address) {
+		// Google maps links for web & iOS
+		// on iOS these open in native Maps app
+		var url = ['http://maps.google.com/maps',
+			'?ll=', lat, ',', lon];
+		if(address) {
+			url.push('&q=' + encodeURIComponent(address));
+		}
+		return url.join('');
+	}
 };
