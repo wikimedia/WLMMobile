56f178399a3bd46ac43ca42d2b747e7fd528f84b
Jon Robson <jdlrobson@gmail.com>
2012-06-19 16:35:48 -0700

    jslint main.js
    use window. for globals
    add jslint header

--- assets/www/js/main.js
+++ assets/www/js/main.js
@@ -1,14 +1,16 @@
+/*global window, onDeviceReady, require*/
+/*jslint sloppy: true, white:true, maxerr: 50, indent: 4, plusplus: true, vars:true */
 // Load Cordova specific stuff here. They all use global variables
 var GLOBAL_LIBS = [];
 if (window.CORDOVA_MODULE !== undefined) {
-	GLOBAL_LIBS.push(CORDOVA_MODULE);
+	GLOBAL_LIBS.push(window.CORDOVA_MODULE);
 }
 if (window.PLATFORM_MODULE !== undefined) {
-	GLOBAL_LIBS.push(PLATFORM_MODULE);
+	GLOBAL_LIBS.push(window.PLATFORM_MODULE);
 }
 GLOBAL_LIBS.push('mediawiki');
 if (window.CORDOVA_PLUGINS !== undefined) {
-	GLOBAL_LIBS = GLOBAL_LIBS.concat(CORDOVA_PLUGINS);
+	GLOBAL_LIBS = GLOBAL_LIBS.concat(window.CORDOVA_PLUGINS);
 }
 require(GLOBAL_LIBS, function() {
 	// deviceready event never fires on Android 4
