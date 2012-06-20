373bbfc0e06d0272baa45132d81e4a6c7320d58d
YuviPanda <yuvipanda@gmail.com>
2012-06-21 03:48:28 +0530

    Toggle between list view and map view seamlessly

--- assets/www/js/geo.js
+++ assets/www/js/geo.js
@@ -2,9 +2,83 @@
 /*jslint sloppy: true, white:true, maxerr: 50, indent: 4, plusplus: true, vars:true */
 define(['jquery', '../leaflet/leaflet-src'], function() {
 
-	var shownURLs = [];
+	var map = null;
+	var markerGroup = null;
 
-	function initMap() {
+	function calculateCenterAndZoom(monuments) {
+		var center = {lat: 0, lon: 0},
+			max = {lat: -999, lon: -999},
+			min = {lat: 999, lon: 999},
+			count = 0,
+			location = {},
+			visible,
+			dist = {lat: 0, lon: 0},
+			zoom = 0;
+		$.each(monuments, function(i, item) {
+			if (item.lat || item.lon) {
+				// Only count things that aren't at (0, 0)
+				if (item.lat < min.lat) {
+					min.lat = item.lat;
+				}
+				if (item.lon < min.lon) {
+					min.lon = item.lon;
+				}
+				if (item.lat > max.lat) {
+					max.lat = item.lat;
+				}
+				if (item.lon > max.lon) {
+					max.lon = item.lon;
+				}
+				count++;
+			}
+		});
+		if (count === 0) {
+			// Seriously?
+			location = {center: {lat: 0, lon: 0}, zoom: 1};
+		} else {
+			center.lat = (min.lat + max.lat) / 2;
+			center.lon = (min.lon + max.lon) / 2;
+			dist.lat = max.lat - min.lat;
+			dist.lon = max.lon - min.lon;
+			dist = Math.max(dist.lat,dist.lon);
+			visible = 360;
+			for (zoom = 1; zoom < 18; zoom++) {
+				visible /= 2;
+				if (dist >= visible) {
+					break;
+				}
+			}
+			location = { center: center, zoom: zoom -1 };
+		}
+		return location;
+	}
+
+	function setupPinchToZoom(id) {
+		if (window.plugins !== undefined && window.plugins.pinchZoom !== undefined && navigator.userAgent.match(/Android 2/)) {
+			var origDistance;
+			window.plugins.pinchZoom.addEventListener('pinchzoom', function(event) {
+				if (map && $("#" + id).is(':visible')) {
+					if(event.type === "pinchzoomstart") {
+						origDistance = event.distance;
+					}
+					else if (event.type === "pinchzoommove" || event.type === "pinchzoomend") {
+						var ratio = event.distance / origDistance;
+						if (ratio < 0.67) {
+							// Zooming out
+							origDistance = event.distance;
+							map.zoomOut();
+						} else if (ratio > 1.5) {
+							// Zooming in
+							origDistance = event.distance;
+							map.zoomIn();
+						}
+					}
+				}
+			});
+		}
+	}
+
+	function init() {
 		if (!this.map) {
 			// Disable webkit 3d CSS transformations for tile positioning
 			// Causes lots of flicker in PhoneGap for some reason...
@@ -13,19 +87,18 @@ define(['jquery', '../leaflet/leaflet-src'], function() {
 				touchZoom: true, // force on for Android 3/4
 				zoomControl: false // disable in favor of pinch-zoom
 			});
-			//var tiles = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 			var tiles = new L.TileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
 				maxZoom: 18,
-				subdomains: '1234' // for MapQuest tiles
+				subdomains: '1234'
 			});
 			this.map.addLayer(tiles);
 
 			this.map.attributionControl.setPrefix("");
 			this.map.attributionControl.addAttribution(
-				'<span class="map-attribution">' + mw.message("attribution-mapquest") + '</span>'
+				'<span class=".map-attribution">' + mw.message("attribution-mapquest") + '</span>'
 				);
 			this.map.attributionControl.addAttribution(
-				'<br /><span class="map-attribution">' + mw.message("attribution-osm") + '</span>'
+				'<br /><span class=".map-attribution">' + mw.message("attribution-osm") + '</span>'
 				);
 
 			$(".map-attribution a").bind('click', function(event) {
@@ -35,32 +108,37 @@ define(['jquery', '../leaflet/leaflet-src'], function() {
 				// chrome.openExternalLink(this.href);
 				event.preventDefault();
 			});
-			this.markerGroup = new L.LayerGroup();
-			this.map.addLayer(this.markerGroup);
+
+			markerGroup = new L.LayerGroup();
+			this.map.addLayer(markerGroup);
+			setupPinchToZoom('map');
 		}
 	}
 	
-	function clearMarkers() {
-		this.markerGroup.clearLayers();
+	function clear() {
+		markerGroup.clearLayers();
 	}
-	
-	function addMarker(lat, lon, title, summary, callback) {
-		console.log('adding marker ' + title);
-		var marker = new L.Marker(new L.LatLng(lat, lon));
-		var html = "<div><strong>" + title + "</strong><p>" + summary + "</p></div>";
-		var popupContent = $(html).click(function() {
-			callback();
+
+	function addMonument(monument, onClick) {
+		var marker = new L.Marker(new L.LatLng(monument.lat, monument.lon));
+		var popup = "<div><strong>" + monument.name + "</strong></div>";
+		var popupDOM = $(popup).click(function() {
+			onClick(monument);
 		})[0];
-		marker.bindPopup(popupContent, {closeButton: false});
-		this.markerGroup.addLayer(marker);
+		marker.bindPopup(popupDOM, {closeButton: false});
+		markerGroup.addLayer(marker);
 	}
-	
+
+	function setCenterAndZoom(center, zoom) {
+		this.map.setView(new L.LatLng(center.lat, center.lon), zoom);
+	}
+
 	return {
-		initMap: initMap,
-		clearMarkers: clearMarkers,
-		addMarker: addMarker,
-		map: null,
-		markerGroup: null
+		init: init,
+		clear: clear,
+		addMonument: addMonument,
+		calculateCenterAndZoom: calculateCenterAndZoom,
+		setCenterAndZoom: setCenterAndZoom
 	};
 
 });
