7cf0045eb9cf018acb3d3df131e0d34bd91af926
Jon Robson <jdlrobson@gmail.com>
2012-06-20 14:09:19 -0700

    jslint monument.js
    rearrange Monument definition for readability

--- assets/www/js/monuments.js
+++ assets/www/js/monuments.js
@@ -1,9 +1,43 @@
+/*global $, JSON, define, platform*/
+/*jslint sloppy: true, white:true, maxerr: 50, indent: 4, plusplus: true, vars:true */
 define(['jquery'], function($) {
 	function stripWikiText(str) {
 		str = str.replace(/\[\[[^\|]+\|([^\]]+)\]\]/g, '$1');
 		str = str.replace(/\[\[([^\]]+)\]\]/g, '$1');
 		return str;
 	}
+
+	function Monument(data, mwApi) {
+		// Sanitize and transform data to be cleaner and more useful
+		var stripFields = ['name', 'address'];
+		var floatFields = ['lat', 'lon'];
+		var that = this;
+		$.each(data, function(key, value) {
+			if($.inArray(key, stripFields) !== -1) {
+				that[key] = stripWikiText(value);
+			} else if($.inArray(key, floatFields) !== -1) {
+				that[key] = parseFloat(value);
+			} else {
+				that[key] = value;
+			}
+		});
+		this.addressLink = platform.geoUrl(this.lat, this.lon, this.address);
+		this.mwApi = mwApi;
+	}
+
+	Monument.prototype.requestThumbnail = function(imageFetcher) {
+		var d = $.Deferred();
+		imageFetcher.request(this.image).done(function(imageinfo) {
+			d.resolve(imageinfo);
+		}).fail(function(err) {
+			d.reject.apply(d, arguments);
+		});
+		return d.promise();
+	};
+
+	Monument.prototype.generateFilename = function() {
+		return this.name + " " + (new Date()).getTime();
+	};
 
 	function MonumentsApi(url, mwApi) {
 		this.url = url;
@@ -51,38 +85,6 @@ define(['jquery'], function($) {
 			action:'search',
 			bbox: bboxString
 		});
-	};
-
-	function Monument(data, mwApi) {
-		// Sanitize and transform data to be cleaner and more useful
-		var stripFields = ['name', 'address'];
-		var floatFields = ['lat', 'lon'];
-		var that = this;
-		$.each(data, function(key, value) {
-			if($.inArray(key, stripFields) !== -1) {
-				that[key] = stripWikiText(value);
-			} else if($.inArray(key, floatFields) !== -1) {
-				that[key] = parseFloat(value);
-			} else {
-				that[key] = value;
-			}
-		});
-		this.addressLink = platform.geoUrl(this.lat, this.lon, this.address);
-		this.mwApi = mwApi;
-	}
-
-	Monument.prototype.requestThumbnail = function(imageFetcher) {
-		var d = $.Deferred();
-		imageFetcher.request(this.image).done(function(imageinfo) {
-			d.resolve(imageinfo);
-		}).fail(function(err) {
-			d.reject.apply(d, arguments);
-		});
-		return d.promise();
-	};
-
-	Monument.prototype.generateFilename = function() {
-		return this.name + " " + (new Date()).getTime();
 	};
 
 	return MonumentsApi;
