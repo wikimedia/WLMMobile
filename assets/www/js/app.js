9e984eeb8f5eaa8a0f0d2c0de170a20ae7979caf
jrobson <jrobson@wikimedia.org>
2012-07-06 10:47:37 -0700

    blacklist location lookup page from history
    when clicking back from a error page we should make sure the user
    gets back to the welcome page instead

--- assets/www/js/app.js
+++ assets/www/js/app.js
@@ -76,7 +76,10 @@ require( [ 'jquery', 'l10n', 'geo', 'api', 'templates', 'monuments', 'preference
 
 	var pageHistory = []; // TODO: retain history
 	function addToHistory( page ) {
-		if( pageHistory[ pageHistory.length - 1 ] !== page ) { // avoid adding the same page twice
+		var blacklist = [ 'locationlookup-page' ];
+		var blacklisted = blacklist.indexOf( page ) > -1;
+		if( !blacklisted &&
+			pageHistory[ pageHistory.length - 1 ] !== page ) { // avoid adding the same page twice
 			pageHistory.push( page );
 		}
 	}
