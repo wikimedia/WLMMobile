835b1526cf1ae36c187397c6b8b292459d9b5a65
YuviPanda <yuvipanda@gmail.com>
2012-07-08 15:29:03 -0400

    Generate the list of campaigns + license + cat information dynamically
    Currently the campaigns *must* have an entry in both UploadWizard
    and in
    http://commons.wikimedia.org/wiki/Commons:Monuments_database/Campaign_names
    for them to show up. The latter part should be eliminated soon, with
    UploadWizard entry being the only criterion

---
+++ assets/www/js/campaigns-data.js
@@ -0,0 +1,203 @@
+window.CAMPAIGNS = [
+    {
+        "licenses": [
+            "cc-by-sa-3.0"
+        ], 
+        "name": "ad", 
+        "categories": [
+            "Cultural heritage monuments in Andorra"
+        ], 
+        "desc": "Andorra"
+    }, 
+    {
+        "licenses": [
+            "cc-by-sa-3.0-at"
+        ], 
+        "name": "at", 
+        "categories": [
+            "Cultural heritage monuments in Austria"
+        ], 
+        "desc": "Austria"
+    }, 
+    {
+        "licenses": [
+            "cc-by-sa-3.0"
+        ], 
+        "name": "be-bru", 
+        "categories": [
+            "Cultural heritage monuments in Brussels"
+        ], 
+        "desc": "Belgium (Brussels)"
+    }, 
+    {
+        "licenses": [
+            "cc-by-sa-3.0"
+        ], 
+        "name": "be-vlg", 
+        "categories": [
+            "Onroerend erfgoed in Flanders"
+        ], 
+        "desc": "Belgium (Flanders)"
+    }, 
+    {
+        "licenses": [
+            "cc-by-sa-3.0"
+        ], 
+        "name": "be-wal", 
+        "categories": [
+            "Cultural heritage monuments in Wallonia"
+        ], 
+        "desc": "Belgium (Wallonia)"
+    }, 
+    {
+        "licenses": [
+            "cc-by-sa-3.0-ee"
+        ], 
+        "name": "ee", 
+        "categories": [
+            "Cultural heritage monuments in Estonia"
+        ], 
+        "desc": "Estonia"
+    }, 
+    {
+        "licenses": [
+            "cc-by-sa-3.0"
+        ], 
+        "name": "fr", 
+        "categories": [
+            "Monuments historiques in France"
+        ], 
+        "desc": "France"
+    }, 
+    {
+        "licenses": [
+            "cc-by-sa-3.0"
+        ], 
+        "name": "de-by", 
+        "categories": [
+            "Images from Wiki Loves Monuments 2011, DE-BY"
+        ], 
+        "desc": "Germany (Bavaria)"
+    }, 
+    {
+        "licenses": [
+            "cc-by-sa-3.0"
+        ], 
+        "name": "de-he", 
+        "categories": [
+            "Images from Wiki Loves Monuments 2011, DE-HE"
+        ], 
+        "desc": "Germany (Hesse)"
+    }, 
+    {
+        "licenses": [
+            "cc-by-sa-3.0"
+        ], 
+        "name": "de-nrw-bm", 
+        "categories": [
+            "Cultural heritage monuments in Bergheim"
+        ], 
+        "desc": "Germany (nrw-bm)"
+    }, 
+    {
+        "licenses": [
+            "cc-by-sa-3.0"
+        ], 
+        "name": "de-nrw-k", 
+        "categories": [
+            "Maintenance for cultural heritage monuments in Cologne"
+        ], 
+        "desc": "Germany (nrw-k)"
+    }, 
+    {
+        "licenses": [
+            "cc-by-sa-3.0-lu"
+        ], 
+        "name": "lu", 
+        "categories": [
+            "Cultural heritage monuments in Luxembourg"
+        ], 
+        "desc": "Luxemburg"
+    }, 
+    {
+        "licenses": [
+            "cc-by-sa-3.0-nl"
+        ], 
+        "name": "nl", 
+        "categories": [
+            "Rijksmonumenten"
+        ], 
+        "desc": "Netherlands"
+    }, 
+    {
+        "licenses": [
+            "cc-by-sa-3.0-no"
+        ], 
+        "name": "no", 
+        "categories": [
+            "Cultural heritage monuments in Norway"
+        ], 
+        "desc": "Norway"
+    }, 
+    {
+        "licenses": [
+            "cc-by-sa-3.0-pl"
+        ], 
+        "name": "pl", 
+        "categories": [], 
+        "desc": "Poland"
+    }, 
+    {
+        "licenses": [
+            "cc-by-sa-3.0"
+        ], 
+        "name": "pt", 
+        "categories": [
+            "Cultural heritage monuments in Portugal"
+        ], 
+        "desc": "Portugal"
+    }, 
+    {
+        "licenses": [
+            "cc-by-sa-3.0-ro"
+        ], 
+        "name": "ro", 
+        "categories": [
+            "Historical_monuments_in_Romania"
+        ], 
+        "desc": "Romania"
+    }, 
+    {
+        "licenses": [
+            "cc-by-sa-3.0", 
+            "cc-by-3.0", 
+            "own-pd", 
+            "cc-by-sa-3.0-es"
+        ], 
+        "name": "es", 
+        "categories": [
+            "Category:Cultural heritage monuments in Spain"
+        ], 
+        "desc": "Spain"
+    }, 
+    {
+        "licenses": [
+            "cc-by-sa-3.0"
+        ], 
+        "name": "se", 
+        "categories": [
+            "Protected buildings in Sweden"
+        ], 
+        "desc": "Sweden"
+    }, 
+    {
+        "licenses": [
+            "cc-by-sa-3.0"
+        ], 
+        "name": "ch", 
+        "categories": [
+            "Cultural properties of national significance in Switzerland"
+        ], 
+        "desc": "Switzerland"
+    }
+]
