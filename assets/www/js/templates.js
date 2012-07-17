e58f14b55d4f987446026dbe613e6df3310a15b5
Arthur Richards <arichards@wikimedia.org>
2012-07-16 18:10:51 -0700

    Makes template caching optional in getTemplate() This is needed for templates like photo description upload which should not get cached.

--- assets/www/js/templates.js
+++ assets/www/js/templates.js
@@ -1,13 +1,21 @@
 // Cache that compiles and stores templates so they don't keep getting recompiled
-// Currently takes templates from index.html
+// Some templates, however, should not be cached (such as photo upload
+// descriptions)
+// Currently takes templates from index.html, and an optional bool param
+// set skipCache = true to prevent requested template from getting cached.
 define(['underscore'], function() {
 	var compiled_templates = {};
-	function getTemplate(name) {
+	function getTemplate(name, skipCache) {
 		if(!compiled_templates[name]) {
 			var html = $("#" + name).html();
-			compiled_templates[name] = _.template(html);
+			compiled_template = _.template(html);
+			if ( skipCache !== true ) {
+				compiled_templates[name] = compiled_template;
+			}
+		} else {
+			compiled_template = compiled_templates[name];
 		}
-		return compiled_templates[name];
+		return compiled_template;
 	}
 
 	return {
