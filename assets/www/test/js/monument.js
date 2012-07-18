c4872e6473683b08c222fbd5471ef5cd8d31ad2d
jrobson <jrobson@wikimedia.org>
2012-07-18 11:07:52 -0700

    deal with other invalid file name characters
    separate Monument function from monuments.js and make available to tests
    write tests to test filename generation

---
+++ assets/www/test/js/monument.js
@@ -0,0 +1,30 @@
+(function() {
+
+module( 'monuments.js', {} );
+
+test( 'generateFilename: . symbol', function() {
+	var data = {
+		lat: 4, lon: 4, name: 'hello . symbol', address: '29 Acacier Road'
+	}, m, m2, m3, m4,
+	name, name2, name3, name4;
+	
+	m = new WLMMobile.Monument( data );
+	data.name = 'hello' + String.fromCharCode(27)+'?';
+	m2 = new WLMMobile.Monument( data );
+	data.name = '[hello]';
+	m3 = new WLMMobile.Monument( data );
+	data.name = '#evil<name>[muh]h|a{hhh}'
+	m4 = new WLMMobile.Monument( data );
+
+	name = m.generateFilename();
+	name2 = m2.generateFilename();
+	name3 = m3.generateFilename();
+	name4 = m4.generateFilename();
+console.log(name2, name2 === 'hello?')
+	strictEqual( name.indexOf( 'hello - symbol' ), 0, 'the illegal symbol was escaped' );
+	strictEqual( name2.indexOf( 'hello-?' ), 0, 'the illegal symbol was escaped' );
+	strictEqual( name3.indexOf( '-hello-' ), 0, 'the illegal symbol was escaped' );
+	strictEqual( name4.indexOf( '-evil-name--muh-h-a-hhh-' ), 0, 'the illegal symbols were escaped' );
+});
+
+}());
