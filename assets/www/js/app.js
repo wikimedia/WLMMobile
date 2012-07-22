1167a31ed85c6c2bcc687191b510665a54c8ef44
YuviPanda <yuvipanda@gmail.com>
2012-07-22 09:18:48 +0530

    Ensure user is logged in before showing them My Uploads

--- assets/www/js/app.js
+++ assets/www/js/app.js
@@ -452,6 +452,7 @@ require( [ 'jquery', 'l10n', 'geo', 'api', 'templates', 'monuments', 'preference
 		return template;
 	}
 
+	// Expects user to be logged in
 	function showUploads() {
 		var username = api.userName;
 		db.requestUploadsForUser( username ).done( function( uploads ) {
@@ -563,8 +564,12 @@ require( [ 'jquery', 'l10n', 'geo', 'api', 'templates', 'monuments', 'preference
 			}, 400);
 		});
 
-		$( '.page-link[data-page="uploads-page"]' ).click( function() {
-			showUploads();
+		$( '#show-uploads' ).click( function() {
+			if( api.loggedIn ) {
+				showUploads();
+			} else {
+				doLogin( showUploads );
+			}
 		} );
 
 		$('#nearby').click(function() {
