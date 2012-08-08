9ea620b691f83366546e3934dce03b6954ea1c58
jrobson <jrobson@wikimedia.org>
2012-08-08 14:33:31 -0700

    separate config from app.js, use http://wlm.wikimedia.org
    this allows forks of this project to easily configure the app to
    their needs and repurpose it in other contexts.
    
    Also bumping over to wlm.wikimedia.org api

---
+++ assets/www/js/config.js
@@ -0,0 +1,5 @@
+var WLMConfig = {
+	COMMONS_API: 'https://commons.wikimedia.org/w/api.php',
+	MONUMENT_API: 'http://wlm.wikimedia.org/api/api.php',
+	WIKI_API: 'https://test.wikipedia.org/w/api.php'
+};
