27d3de50c745867ec6aaaf962fc99294c83f5feb
awjrichards <arichards@wikimedia.org>
2012-08-24 14:40:56 -0700

    Switch file uploads from testwiki to commons

--- assets/www/js/config.js
+++ assets/www/js/config.js
@@ -4,8 +4,8 @@ var WLMConfig = {
 	MONUMENT_API: 'http://wlm.wikimedia.org/api/api.php',
 	MONUMENT_SEARCH_LIMIT: 50, // amount of monuments to request in a single query (see bug 39182 for more context)
 	SIGNUP_PAGE: 'https://commons.wikimedia.org/w/index.php?title=Special:UserLogin&type=signup&uselang=$1',
-	WIKI_API: 'https://test.wikipedia.org/w/api.php',
-	WIKI_BASE: 'https://test.wikipedia.org/wiki/',
+	WIKI_API: 'https://commons.wikimedia.org/w/api.php',
+	WIKI_BASE: 'https://commons.wikimedia.org/wiki/',
 	WIKI_LOVES_MONUMENTS_HOMEPAGE: 'http://www.wikilovesmonuments.org/',
 	WIKIPEDIA: 'https://$1.wikipedia.org',
 	WIKIPEDIA_API: 'https://$1.wikipedia.org/w/api.php',
