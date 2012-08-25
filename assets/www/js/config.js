798e1f96a169a1a0385df5735ecca4d99aa2f1cc
awjrichards <arichards@wikimedia.org>
2012-08-24 18:32:23 -0700

    Fix config typo

--- assets/www/js/config.js
+++ assets/www/js/config.js
@@ -4,7 +4,7 @@ var WLMConfig = {
 	MONUMENT_API: 'http://wlm.wikimedia.org/api/api.php',
 	MONUMENT_SEARCH_LIMIT: 50, // amount of monuments to request in a single query (see bug 39182 for more context)
 	SIGNUP_PAGE: 'https://commons.wikimedia.org/w/index.php?title=Special:UserLogin&type=signup&uselang=$1',
-	WIKI_API: 'https://commons.wikipedia.org/w/api.php',
+	WIKI_API: 'https://commons.wikimedia.org/w/api.php',
 	WIKI_LOVES_MONUMENTS_HOMEPAGE: 'http://www.wikilovesmonuments.org/',
 	WIKIPEDIA: 'https://$1.wikipedia.org',
 	WIKIPEDIA_API: 'https://$1.wikipedia.org/w/api.php',
