d1934ba66b91ed947b4b2f73ae8dde348ea56d5a
Brion Vibber <brion@pobox.com>
2012-08-28 00:34:07 -0500

    Bug 39571: truncate long filenames
    * Adds trimUtf8String function to utils.js, with some test cases.
    * strip more evil chars from filenames
    * changes filename format to "[truncated filtered name] YYYY-MM-DD HH-MM-SS.jpg"
    * truncates to fit filenames in 240 UTF-8 bytes, cut in half, with extra wiggle room for thumb stuff
    
    Includes a test case with a super long monument name from Belarus, and some quick tests for the truncation itself.

--- assets/www/js/utils.js
+++ assets/www/js/utils.js
@@ -5,3 +5,43 @@ function stripWikiText( str ) {
 	str = str.replace( /\{\{([^\]]+)\}\}/g, '' );
 	return str;
 }
+
+function trimUtf8String( str, allowedLength ) {
+	// Count UTF-8 bytes to see where we need to crop long names.
+	var bytes = 0, chars = 0;
+	var codeUnit, len;
+
+	for ( var i = 0; i < str.length; i++ ) {
+		// JavaScript strings are UTF-16.
+		codeUnit = str.charCodeAt( i );
+
+		// http://en.wikipedia.org/wiki/UTF-8#Description
+		if ( codeUnit < 0x80 ) {
+			len = 1;
+		} else if ( codeUnit < 0x800 ) {
+			len = 2;
+		} else if ( codeUnit >= 0xd800 && codeUnit < 0xe000 ) {
+			// http://en.wikipedia.org/wiki/UTF-16#Description
+			// Code point is one half of a surrogate pair.
+			// This and its partner combine to form a single 4 byte character in UTF-8.
+			len = 4;
+		} else {
+			len = 3;
+		}
+		if ( bytes + len <= allowedLength ) {
+			bytes += len;
+			chars++;
+			if ( len == 4 ) {
+				// Skip over second half of surrogate pair as a unit.
+				chars++;
+				i++;
+			}
+		} else {
+			// Ran out of bytes.
+			return str.substr( 0, chars );
+		}
+	}
+
+	// We fit!
+	return str;
+}
