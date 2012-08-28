cb602b64d1a1113a3e8c8dca7f6945c6b57d744f
Brion Vibber <brion@pobox.com>
2012-08-28 00:34:07 -0500

    Bug 39571: truncate long filenames
    * strip more evil chars
    * changes filename format to "[truncated filtered name] DD-MM-YY HH-MM-SS.jpg"
    * truncates to fit filenames in 240 UTF-8 bytes, cut in half, with extra wiggle room for thumb stuff
    
    Includes a test case with a super long monument name from Belarus.

--- assets/www/js/monument.js
+++ assets/www/js/monument.js
@@ -33,13 +33,76 @@ define( [ 'jquery', 'utils' ], function() {
 	};
 
 	Monument.prototype.generateFilename = function( date ) {
+		var maxLength = 240; // 240 bytes maximum enforced by MediaWiki
+		maxLength = maxLength / 2 - '/1024px-'.length; // halve space due to current bugs with Swift causing thumbnails to fail
+
 		var name = this.name.replace( String.fromCharCode(27), '-' );
+		name = name.replace( /[\x7f\.\[#<>\[\]\|\{\}/:]/g, '-' );
+
 		var d = date || new Date();
-		var months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec' ];
-		var suffix =  d.getDate() + months[ d.getMonth() ] + d.getFullYear() + ' ' + d.getHours() + 'hrs' + d.getMinutes() + 'mins' + d.getSeconds() + 'secs';
-		return name.replace( /[\x7f\.\[#<>\[\]\|\{\}]/g, '-' ) + ' (taken on ' + suffix + ').jpg';
+		function pad(str, len) {
+			if ( typeof str != 'string' ) {
+				str = str + '';
+			}
+			while ( str.length < len ) {
+				str = '0' + str;
+			}
+			return str;
+		}
+		var suffix = ' ' +
+			pad( d.getDate(), 2 ) +
+			'-' +
+			pad( d.getMonth(), 2 ) +
+			'-' +
+			pad( d.getFullYear(), 4 ) +
+			' ' +
+			pad( d.getHours(), 2 ) +
+			'-' +
+			pad( d.getMinutes(), 2 ) +
+			'-' +
+			pad( d.getSeconds(), 2 ) +
+			'.jpg';
+
+		var allowedLength = maxLength - suffix.length;
+
+		// Count UTF-8 bytes to see where we need to crop long names.
+		var bytes = 0, chars = 0;
+		for ( var i = 0; i < name.length; i++ ) {
+			// JavaScript strings are UTF-16.
+			var codeUnit = name.charCodeAt( i );
+			var len;
+
+			// http://en.wikipedia.org/wiki/UTF-8#Description
+			if ( codeUnit < 0x80 ) {
+				len = 1;
+			} else if ( codeUnit < 0x800 ) {
+				len = 2;
+			} else if ( codeUnit >= 0xd800 && codePoint < 0xe000 ) {
+				// http://en.wikipedia.org/wiki/UTF-16#Description
+				// Code point is one half of a surrogate pair.
+				// This and its partner combine to form a single 4 byte character in UTF-8.
+				len = 4;
+				i++;
+			} else {
+				len = 3;
+			}
+			if ( bytes + len < allowedLength ) {
+				bytes += len;
+				chars++;
+				if ( len == 4 ) {
+					// Skip over second half of surrogate pair as a unit.
+					chars++;
+					i++;
+				}
+			} else {
+				break;
+			}
+		}
+
+		var base = name.substr( 0, chars );
+		return base + suffix;
 	};
-	
+
 	Monument.prototype.processArticleLink = function() {
 		if ( this.monument_article ) {
 			return 'https://' + this.lang + '.wikipedia.org/wiki/' + encodeURIComponent( this.monument_article );
--- assets/www/test/js/monument.js
+++ assets/www/test/js/monument.js
@@ -7,23 +7,31 @@ test( 'generateFilename: . symbol', function() {
 		lat: 4, lon: 4, name: 'hello . symbol', address: '29 Acacier Road'
 	}, m, m2, m3, m4,
 	name, name2, name3, name4;
+
+	var maxLength = 255;
+	var paddingLength = '1024px-'.length; // some things get wonky if we don't leave room for thumbs
 	
 	m = new WLMMobile.Monument( data );
 	data.name = 'hello' + String.fromCharCode(27)+'?';
 	m2 = new WLMMobile.Monument( data );
 	data.name = '[hello]';
 	m3 = new WLMMobile.Monument( data );
-	data.name = '#evil<name>[muh]h|a{hhh}'
+	data.name = '#evil<name>[muh]h|a{hhh}oh:no/mr.bill'
 	m4 = new WLMMobile.Monument( data );
+	data.name = 'Аўгустоўскі канал:водны шлях, які ўключае зарэгуляваны ўчастак р. Чорная Ганча ад Дзяржаўнай граніцы з Рэспублікай Польшча да в. Сонічы (13км), штучны ўчастак канала ад в. Сонічы да шлюза«Нямнова» (6,5 км)гідравузел «Нямнова»: Гарадзенскі раён. Тэрыторыя ў межах матэрыяльнай нерухомайгісторыка-культурнай каштоўнасьці, устаноўленых праектам «Зоны аховы і рэжымыіх утрыманьня гісторыка-культурнай каштоўнасьці «Аўгустоўскі канал»';
+	m5 = new WLMMobile.Monument( data );
 
 	name = m.generateFilename();
 	name2 = m2.generateFilename();
 	name3 = m3.generateFilename();
 	name4 = m4.generateFilename();
+	name5 = m5.generateFilename();
 	strictEqual( name.indexOf( 'hello - symbol' ), 0, 'the illegal symbol was escaped' );
 	strictEqual( name2.indexOf( 'hello-?' ), 0, 'the illegal symbol was escaped' );
 	strictEqual( name3.indexOf( '-hello-' ), 0, 'the illegal symbol was escaped' );
-	strictEqual( name4.indexOf( '-evil-name--muh-h-a-hhh-' ), 0, 'the illegal symbols were escaped' );
+	strictEqual( name4.indexOf( '-evil-name--muh-h-a-hhh-oh-no-mr-bill' ), 0, 'the illegal symbols were escaped' );
+	ok( name5.length < 240, 'long filenames are truncated with room to under 240 chars' ); // warning: actual limit lowered due to thumbnail problems
+	strictEqual( name5.length, 71, 'long filenames are truncated for utf-8' ); // ideally this would convert to utf-8 and check the buffer length :)
 });
 
 test( 'randomness of monument names', function() {
