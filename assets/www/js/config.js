986cee7bc9842f2a1d2cc72a7fea5e4704404eff
jdlrobson <jrobson@wikimedia.org>
2012-09-24 11:48:10 -0700

    RELEASE 1.2.5
    point to commons rather than test

--- assets/www/js/config.js
+++ assets/www/js/config.js
@@ -5,13 +5,13 @@ var WLMConfig = {
 	MONUMENT_API: 'http://wlm.wikimedia.org/api/api.php',
 	MONUMENT_SEARCH_LIMIT: 50, // amount of monuments to request in a single query (see bug 39182 for more context)
 	SIGNUP_PAGE: 'https://commons.wikimedia.org/w/index.php?title=Special:UserLogin&type=signup&uselang=$1',
-	WIKI_API: 'https://test.wikipedia.org/w/api.php',
-	WIKI_BASE: 'https://test.wikipedia.org/wiki/',
+	WIKI_API: 'https://commons.wikimedia.org/w/api.php',
+	WIKI_BASE: 'https://commons.wikimedia.org/wiki/',
 	WIKI_LOVES_MONUMENTS_HOMEPAGE: 'http://www.wikilovesmonuments.org/',
 	WIKIPEDIA: 'https://$1.wikipedia.org',
 	WIKIPEDIA_API: 'https://$1.wikipedia.org/w/api.php',
-	VERSION_NUMBER: '1.2.4',
-	VERSION_NAME: 'Daigo-ji', // famous monument name (next should begin with D)
+	VERSION_NUMBER: '1.2.5',
+	VERSION_NAME: 'Eiffel Tower', // famous monument name (next should begin with F)
 
 	// app specific
 	THUMB_SIZE: 64 * window.devicePixelRatio
