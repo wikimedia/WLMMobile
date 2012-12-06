a6d10d3befc104205553ba1d2e35421f0630d0eb
Keith C. Ivey <keith@iveys.org>
2012-12-06 11:43:12 -0500

    change MONUMENT_API to new URL to avoid redirect garbling URL parameter encoding

--- assets/www/js/config.js
+++ assets/www/js/config.js
@@ -2,7 +2,7 @@ var WLMConfig = {
 	BLOCKING_POLICY: 'http://commons.wikimedia.org/wiki/Commons:Blocking_policy?uselang=$1',
 	COMMONS_API: 'https://commons.wikimedia.org/w/api.php',
 	GITHUB_MESSAGES: 'https://raw.github.com/wikimedia/WLMMobile/master/assets/www/',
-	MONUMENT_API: 'http://wlm.wikimedia.org/api/api.php',
+	MONUMENT_API: 'http://toolserver.org/~erfgoed/api/api.php',
 	MONUMENT_SEARCH_LIMIT: 50, // amount of monuments to request in a single query (see bug 39182 for more context)
 	SIGNUP_PAGE: 'https://commons.wikimedia.org/w/index.php?title=Special:UserLogin&type=signup&uselang=$1',
 	WIKI_API: 'https://test.wikipedia.org/w/api.php',
